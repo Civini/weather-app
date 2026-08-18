02b942cd64426432e7989de0d08c348013e71f62
kanthi <kcivini@gmail.com>
2026-08-18 11:40:25 +0530

    Fix ForecastPage filename casing

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from "./pages/RegisterPage";
 
 import HomePage from "./pages/HomePage";
-import ForecastPage from "./pages/ForecastPage";
+import ForeCastPage from "./pages/ForeCastPage";
 import HourlyPage from "./pages/HourlyPage";
 import ChartPage from "./pages/ChartPage";
 import SunrisePage from "./pages/SunrisePage";
