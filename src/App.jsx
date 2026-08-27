b776f4408587b9313999392af4c9ea8a53c8341e
kanthi <kcivini@gmail.com>
2026-08-27 10:51:24 +0530

    Fix useWeather import case

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import FavoritesPage from "./pages/FavoritesPage";
 import ChatPage from "./pages/ChatPage";
 
 import useFavorites from "./hooks/useFavorites";
-import useWeather from "./hooks/useweather";
+import useWeather from "./hooks/useWeather";
 
 
 function AuthenticatedApp() {
